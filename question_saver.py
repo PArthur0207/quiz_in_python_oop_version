1c3b7b388b6e7568f9d6fa0b3645e6bd7894be49
PArthur0207 <marayanpaularthur1114@gmail.com>
2025-05-22 12:01:06 +0800

    Fixed Variable name

--- question_saver.py
+++ question_saver.py
@@ -12,12 +12,12 @@ class QuestionSaver():
             return "Answer must be A, B, C, or D"
 
         # Append the questions to be put into the text file
-        with open(self.file_name, "a") as f:
-            f.write(f"Question: {data['Question']}\n")
-            f.write(f"A.) {data['A']}\n")
-            f.write(f"B.) {data['B']}\n")
-            f.write(f"C.) {data['C']}\n")
-            f.write(f"D.) {data['D']}\n")
-            f.write(f"Answer: {answer}\n")
-            f.write("------------------------\n")
+        with open(self.file_name, "a") as file:
+            file.write(f"Question: {data['Question']}\n")
+            file.write(f"A.) {data['A']}\n")
+            file.write(f"B.) {data['B']}\n")
+            file.write(f"C.) {data['C']}\n")
+            file.write(f"D.) {data['D']}\n")
+            file.write(f"Answer: {answer}\n")
+            file.write("------------------------\n")
         return "success"
